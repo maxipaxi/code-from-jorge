c0133a1b9810cedaf8a3feca2f69ef730147ffdf
Christian Clausen <maxclausen@gmail.com>
2020-08-04 22:30:25 +0200

    Extract if

--- Main.java
+++ Main.java
@@ -1,18 +1,21 @@
 public class Main {
   void buildQueryAndProcessFunnyStuff(String dataString) { }
+  void processIfFullChunk(int counter, StringBuffer dataString){
+    if (counter % 20000 == 0) {
+      // Process 20K chunk
+      buildQueryAndProcessFunnyStuff( dataString.toString() );
+
+      dataString = new StringBuffer();
+      counter = 0; // Reset the Counter
+    }
+  }
   void processByChunks(String[] paramArray) {
     StringBuffer dataString = new StringBuffer();
     int counter = 0;
     for (String param : paramArray) {
         dataString.append( param ).append(",");
         counter++;
-        if (counter % 20000 == 0) {
-          // Process 20K chunk
-          buildQueryAndProcessFunnyStuff( dataString.toString() );
-  
-          dataString = new StringBuffer();
-          counter = 0; // Reset the Counter
-        }
+        processIfFullChunk(counter, dataString);
     }
   
     /*
