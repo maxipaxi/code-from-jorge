6aaf8d84de2cfa7aaf5dfd8e6d8840cfd84ce5c6
Christian Clausen <maxclausen@gmail.com>
2020-08-04 22:35:18 +0200

    Encapsulate methods

--- Main.java
+++ Main.java
@@ -1,36 +1,39 @@
 public class Main {
   void buildQueryAndProcessFunnyStuff(String dataString) { }
-  void processIfFullChunk(int counter, StringBuffer dataString){
-    if (counter % 20000 == 0) {
-      // Process 20K chunk
-      buildQueryAndProcessFunnyStuff( dataString.toString() );
-
-      dataString = new StringBuffer();
-      counter = 0; // Reset the Counter
-    }
-  }
-  void finalizeChunk(StringBuffer dataString){
-    /*
-    * WARNING: Code duplication
-    * 
-    * For the remaining beyond %20K
-    */
-    if (dataString!= null && dataString.length() > 0) {
+  private class Chunk {
+    void processIfFull(int counter, StringBuffer dataString){
+      if (counter % 20000 == 0) {
+        // Process 20K chunk
         buildQueryAndProcessFunnyStuff( dataString.toString() );
+
+        dataString = new StringBuffer();
+        counter = 0; // Reset the Counter
+      }
     }
-  }
-  void addElementToChunk(String element, int counter, StringBuffer dataString){
-    dataString.append( element ).append(",");
-    counter++;
-    processIfFullChunk(counter, dataString);
+    void finalizeChunk(StringBuffer dataString){
+      /*
+      * WARNING: Code duplication
+      * 
+      * For the remaining beyond %20K
+      */
+      if (dataString!= null && dataString.length() > 0) {
+          buildQueryAndProcessFunnyStuff( dataString.toString() );
+      }
+    }
+    void addElement(String element, int counter, StringBuffer dataString){
+      dataString.append( element ).append(",");
+      counter++;
+      processIfFull(counter, dataString);
+    }
   }
   void processByChunks(String[] paramArray) {
     StringBuffer dataString = new StringBuffer();
     int counter = 0;
+    Chunk chunk = new Chunk();
     for (String param : paramArray) {
-      addElementToChunk(param, counter, dataString);
+      chunk.addElement(param, counter, dataString);
     }
-    finalizeChunk(dataString);
+    chunk.finalizeChunk(dataString);
   }
   public static void main(String[] args){
 
