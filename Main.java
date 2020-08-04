9dbab67bde6d9a30b0a80531541a5f50970823ec
Christian Clausen <maxclausen@gmail.com>
2020-08-04 22:27:21 +0200

    Initial code

---
+++ Main.java
@@ -0,0 +1,30 @@
+public class Main {
+  void buildQueryAndProcessFunnyStuff(String dataString) { }
+  void processByChunks(String[] paramArray) {
+    StringBuffer dataString = new StringBuffer();
+    int counter = 0;
+    for (String param : paramArray) {
+        dataString.append( param ).append(",");
+        counter++;
+        if (counter % 20000 == 0) {
+          // Process 20K chunk
+          buildQueryAndProcessFunnyStuff( dataString.toString() );
+  
+          dataString = new StringBuffer();
+          counter = 0; // Reset the Counter
+        }
+    }
+  
+    /*
+    * WARNING: Code duplication
+    * 
+    * For the remaining beyond %20K
+    */
+    if (dataString!= null && dataString.length() > 0) {
+        buildQueryAndProcessFunnyStuff( dataString.toString() );
+    }
+  }
+  public static void main(String[] args){
+
+  }
+}
